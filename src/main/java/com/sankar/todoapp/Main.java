464390d335a5c756e435559c063e25ae2dc925e0
Sankaranarayanan Viswanathan <rationalrevolt@gmail.com>
2014-07-26 02:00:52 -0400

    Runnable jar with embedded-tomcat

--- src/main/java/com/sankar/todoapp/Main.java
+++ src/main/java/com/sankar/todoapp/Main.java
@@ -1,5 +1,27 @@
 package com.sankar.todoapp;
 
+import java.io.File;
+import org.apache.catalina.startup.Tomcat;
+
 public class Main {
 
+    public static void main(String[] args) throws Exception {
+
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        tomcat.addWebapp("/todo-app", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+    
 }
